cf9021494b2e0820555d6f2cc1d6bc53f907792a
FrancescoCesari97 <freedus97@gmail.com>
2024-03-04 16:43:05 +0100

    aggiunte le propietà delle produzioni tramite construct

--- index.php
+++ index.php
@@ -7,13 +7,34 @@ class Production
     public $lingua;
     public $voto;
 
-    function __construct(
-        string $titolo,
-        string $lingua,
-        int $voto
-    ) {
+
+    function __construct($titolo, $lingua, $voto)
+    {
         $this->titolo = $titolo;
         $this->lingua = $lingua;
         $this->voto = $voto;
     }
+
+    //* Metodo per recuperare il titolo
+    public function getTitolo()
+    {
+        return $this->titolo;
+    }
+
+
 }
+
+$amicimiei = new Production("Amici Miei", "Italiano", "8");
+// $amicimiei->titolo = "Amici Miei";
+// $amicimiei->lingua = "Italiano";
+// $amicimiei->voto = "8";
+
+$heat = new Production("Heat", "Inglese", "7");
+// $heat->titolo = "Heat";
+// $heat->lingua = "Inglese";
+// $heat->voto = "7";
+
+
+var_dump($amicimiei);
+
+var_dump($heat);
