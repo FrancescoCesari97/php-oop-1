19711d933e6aaab17b786a245a6b6edfbca8a7d2
FrancescoCesari97 <freedus97@gmail.com>
2024-03-04 14:53:01 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Production 
+{
+    public $titolo;
+    public $lingua;
+    public $voto;
+
+    function __construct(
+        string $titolo,
+        string $lingua,
+        int $voto
+    ) {
+        $this->titolo = $titolo;
+        $this->lingua = $lingua;
+        $this->voto = $voto;
+    }
+}
